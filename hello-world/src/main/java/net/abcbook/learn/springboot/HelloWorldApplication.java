4d121eeead65e51e7a37c242d4f377a3487c5540
summer <lixian@tinypace.com>
2018-04-03 16:13:00 +0800

    创建 Maven 结构并创建第一个 HelloWorld 项目
    创建 Maven 结构并创建第一个 HelloWorld 项目

---
+++ hello-world/src/main/java/net/abcbook/learn/springboot/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package net.abcbook.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
